67a800eda8bbc75613a1d38aa7073d95e32e4999
Kenneth Centurion <kcenturion@yahoo.com>
2016-05-08 01:11:24 -0500

    i

--- pbooth.py
+++ pbooth.py
@@ -12,19 +12,19 @@ with picamera.PiCamera() as camera:
 
     try:
         camera.start_preview()
-        time.sleep(10)
-        camera.stop_preview()
+        #time.sleep(10)
+        #camera.stop_preview()
     finally:
         camera.close()
 
-    #overlay text on output
-
     #save image
-    #camera.capture('love.jpg')
+    camera.capture('love.jpg')
+
+    subprocess.call(['convert -font /usr/share/fonts/truetype/droid/DroidSerif-Italic.ttf -pointsize 40 -fill HotPink2 -draw \'text 90,660 \\"Nadine & Kenneth - July 23rd, 2016 - Dreams Tulum - Mexico\\" \' love.jpg love1.jpg
+'])
 
     #print
     #conn = cups.Connection()
     #printers = conn.getPrinters ()
     #printer_name=printers.keys()[0]
     #conn.printFile (printer_name, file, "test", {})
-
