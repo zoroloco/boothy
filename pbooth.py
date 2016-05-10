81cb1440d79cf088659fb9b053f15b6353429212
Kenneth Centurion <kcenturion@yahoo.com>
2016-05-09 23:51:08 -0500

    threading start

--- pbooth.py
+++ pbooth.py
@@ -2,9 +2,30 @@ wimport picamera
 import time
 import itertools
 import cups
+import threading
 
 from PIL import Image
 from time import sleep
+
+class overlayText(threading.Thread):
+    def __init__(self, threadID):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+    def run(self):
+        subprocess.call(["convert",
+                         "-font", "/usr/share/fonts/truetype/droid/DroidSerif-Italic.ttf",
+                         "-pointsize", "40",
+                         "-fill", "HotPink2",
+                         "-draw" "'text 90,660 \"Nadine & Kenneth - July 23rd, 2016 - Dreams Tulum - Mexico\" '",
+                         "temp"+threadID+".jpg", threadID+".jpg"])
+
+def printPic(){
+    #print
+    #conn = cups.Connection()
+    #printers = conn.getPrinters ()
+    #printer_name=printers.keys()[0]
+    #conn.printFile (printer_name, file, "test", {})
+}
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
@@ -12,24 +33,18 @@ with picamera.PiCamera() as camera:
 
     try:
         camera.start_preview()
-        #time.sleep(10)
-        #camera.stop_preview()
 
-        #save image
-        camera.capture('love.jpg')
+        index = 0;
+        while index < 2:
+            #save image
+            camera.capture("temp"+index+'.jpg')
+            overlayTextThread = overlayText(index, index)
+            overlayTextThread.start()
+            index++
+            time.sleep(1)
 
-        subprocess.call(["convert",
-                         "-font", "/usr/share/fonts/truetype/droid/DroidSerif-Italic.ttf",
-                         "-pointsize", "40",
-                         "-fill", "HotPink2",
-                         "-draw" "'text 90,660 \"Nadine & Kenneth - July 23rd, 2016 - Dreams Tulum - Mexico\" '",
-                         "love.jpg", "love1.jpg"])
+        camera.stop_preview()
 
-        #print
-        #conn = cups.Connection()
-        #printers = conn.getPrinters ()
-        #printer_name=printers.keys()[0]
-        #conn.printFile (printer_name, file, "test", {})
-
+        #printPic()
     finally:
         camera.close()
