ad213b4c2a0b365c657ec9a9ace654d9fd09da49
Kenneth Centurion <kcenturion@yahoo.com>
2016-05-16 23:36:41 -0500

    got the subprocesses to work in python.
    file path fixed too.

--- pbooth.py
+++ pbooth.py
@@ -7,24 +7,37 @@ from PIL import Image
 from time import sleep
 
 filePath = "/mnt/share/boothy/"
+tempFilePath = "/tmp/"
 IMAGE_WIDTH  = 640
 IMAGE_HEIGHT = 480
+fileName = "love_final.jpg"
 
 def printPic():
     conn = cups.Connection()
     printers = conn.getPrinters ()
     printer_name=printers.keys()[0]
-    conn.printFile (printer_name, file, "test", {})
+    conn.printFile (printer_name, file, filePath+"love_montage.jpg", {})
+    print filePath+fileName+" sent to printer."
 
 #merges the three images
 def convertMergeImages():
     #create a dummy blank
-    #subprocess.call(["convert -size 640x480 xc:white "+filePath+"3.jpg"])
+    subprocess.call(["convert",
+                     "-size", "640x480",
+                     "xc:white",
+                     tempFilePath+"3.jpg"])
     #now check in a loop if the 3.jpg file exists. if so, then
     sleep(2)
     #now merge all the images
-    #subprocess.call(["montage " filePath+"0.jpg" filePath+"1.jpg" filePath+"2.jpg" filePath+"3.jpg" -geometry +2+2 /mnt/share/boothy/love_montage.jpg"])
-    subprocess.call(["/usr/local/src/boothy/merge.sh"])
+    print "Blank 4th image created."
+    subprocess.call(["montage",
+                     tempFilePath+"0.jpg",
+                     tempFilePath+"1.jpg",
+                     tempFilePath+"2.jpg",
+                     tempFilePath+"3.jpg",
+                     "-geometry", "+2+2",
+                     tempFilePath+"love_montage.jpg"])
+    print "Images have been merged."
 
 #adds the text to final image.
 def convertAddText():
@@ -32,8 +45,9 @@ def convertAddText():
                 "-font", "/usr/share/fonts/truetype/droid/DroidSerif-Italic.ttf",
                 "-pointsize", "40",
                 "-fill", "HotPink2",
-                "-draw" "'text 90,660 \"Nadine & Kenneth - July 23rd, 2016 - Dreams Tulum - Mexico\" '",
-                "/mnt/share/boothy/love_montage.jpg", "/mnt/share/boothy/love_final.jpg"])
+                "-draw", "text 90,660 \'Nadine & Kenneth - July 23rd, 2016 - Dreams Tulum - Mexico\'",
+                tempFilePath+"love_montage.jpg", filePath+fileName])
+    print "Text overlay added to image."
 
 def captureImage(imageName):
     with picamera.PiCamera() as camera:
@@ -44,14 +58,16 @@ def captureImage(imageName):
             camera.start_preview()
             #save image
             camera.capture(imageName, resize=(IMAGE_WIDTH, IMAGE_HEIGHT))
+            print "Image "+imageName+" captured."
         finally:
             camera.close()
 
-captureImage(filePath+"0.jpg")
+
+captureImage(tempFilePath+"0.jpg")
 sleep(1)
-captureImage(filePath+"1.jpg")
+captureImage(tempFilePath+"1.jpg")
 sleep(1)
-captureImage(filePath+"2.jpg")
+captureImage(tempFilePath+"2.jpg")
 sleep(1)
 convertMergeImages()
 sleep(2)
