dfc63f5a7374a3b7eb96cb1fe3ac17f7e1b48506
Kenneth Centurion <kcenturion@yahoo.com>
2016-07-07 07:56:15 -0500

    d

--- pbooth.py
+++ pbooth.py
@@ -146,19 +146,19 @@ def initLogger(output_dir):
     # create console handler and set level to info
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("[%(time.strftime("%Y%m%d-%H%M%S"))] - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     # create error file handler and set level to error
     handler = logging.FileHandler(os.path.join(output_dir, "error.log"),"w", encoding=None, delay="true")
     handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("[%(time.strftime("%Y%m%d-%H%M%S"))] - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     # create debug file handler and set level to debug
     handler = logging.FileHandler(os.path.join(output_dir, time.strftime("%Y%m%d")+"_boothy.log"),"w")
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("["+time.strftime("%Y%m%d-%H%M%S")+"]:"+%(levelname)s - %(message)s")
+    formatter = logging.Formatter("[%(time.strftime("%Y%m%d-%H%M%S"))] - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
