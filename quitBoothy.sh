b50fca776fd3dd7e24d4ebebca37a2091425d2fa
Kenneth Centurion <kcenturion@yahoo.com>
2016-07-14 08:04:18 -0500

    start-stop scripts to place on desktop

---
+++ quitBoothy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chmod +x quitboothy.sh
+#
+# This script will kill boothy
+#
+
+sudo /etc/init.d/boothyStart.sh stop
---
+++ startBoothy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# chmod +x quitboothy.sh
+#
+# This script will start boothy
+#
+
+sudo /etc/init.d/boothyStart.sh start
